79dc34e9e4f6475a6321198c8b27d927ece7a738
osnomkar <osnomkar@gmail.com>
2017-06-25 03:11:21 +0530

    onClick upper n lower

--- uixml/src/main/java/com/conq/omkar/uixml/MainActivity.java
+++ uixml/src/main/java/com/conq/omkar/uixml/MainActivity.java
@@ -2,6 +2,9 @@ package com.conq.omkar.uixml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnU = (Button) findViewById(R.id.btnU);
+        btnU.setOnClickListener(new Upper());
+        Button btnL = (Button) findViewById(R.id.btnL);
+        btnL.setOnClickListener(new Lower());
+
     }
+
+    private class Upper implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            EditText edttxt = (EditText) findViewById(R.id.edttxt);
+            String dt = edttxt.getText().toString();
+            edttxt.setText(dt.toUpperCase());
+        }
+    }
+
+   private class Lower implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            EditText edttxt = (EditText) findViewById(R.id.edttxt);
+            String dt = edttxt.getText().toString();
+            edttxt.setText(dt.toLowerCase());
+        }
+       }
+
 }
